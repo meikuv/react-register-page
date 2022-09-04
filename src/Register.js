a92cbb0d816f69a1ed2fb1c8fdf57f2792bd5f2d
devMvedS <mega.kulmanov@list.ru>
2022-09-04 23:48:10 +0600

    delete validator row

--- src/Register.js
+++ src/Register.js
@@ -1,7 +1,6 @@
 import { useState, useEffect, useRef } from 'react';
 import { faCheck, faTimes, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import validator from 'validator';
 
 const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
