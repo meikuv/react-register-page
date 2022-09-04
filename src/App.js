a2763b26ba9a9f4e35c3cb562a84e4e490b9665e
devMvedS <mega.kulmanov@list.ru>
2022-09-04 22:36:24 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react';
+import Register from './Register';
+
+function App() {
+  return (
+    <div className="App">
+      <Register />
+    </div>
+  );
+}
+
+export default App;
